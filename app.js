b493419ea3c44085a750891ea5c03adb5e372787
Amadou <xalima99@gmail.com>
2020-06-02 13:46:16 +0000

    fixed

--- app.js
+++ app.js
@@ -16,14 +16,15 @@ const app = express();
 const questions = require('./server/routes/api/questions');
 app.use('/api/questions', questions);
 
+// console.log(process.env.NODE_ENV)
 //handle prod
-if(process.env.NODE_ENV == 'production'){
+// if(process.env.NODE_ENV == 'production'){
   //static folder
-  app.use(express.static(__dirname + 'server/public/'));
+  app.use(express.static(__dirname + '/server/public/'));
 
   //handle SPA
-  app.get(/.*/, (req, res) => res.sendFile(__dirname + '/public/index.html'))
-}
+  app.get(/.*/, (req, res) => res.sendFile(__dirname + '/server/public/index.html'))
+// }
 
 const port = process.env.PORT || 4000;
 
